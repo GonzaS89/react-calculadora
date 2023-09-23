728f7f2799fb71c4c7fab0905ea4adaffa1f4c38
Gonza <gnzx89@gmail.com>
2023-09-23 11:05:11 -0300

    a

---
+++ src/componentes/Pantalla.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../hojasestilo/pantalla.css'
+
+function Pantalla () {
+    return (
+        <div className='pantalla'>
+
+        </div>
+    )
+}
+
+export default Pantalla;
